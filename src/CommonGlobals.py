79c7f1e144122ed57ea7b29d6f43a10c69aee820
Moreau <moreauo@wit.edu>
2019-11-29 17:32:45 -0500

    Add common globals

--- src/CommonGlobals.py
+++ src/CommonGlobals.py
@@ -7,4 +7,21 @@ Fri, Oct 25, 2019
 :authors:
     Owen Moreau, Jian Huang, Mario La
 """
-DEV_SERVER_ADDR = "10.220.74.108"
+import datetime
+
+
+class Message:
+    def __init__(self, user):
+        self.message = input(" -> ")
+        self.time = datetime.datetime.now()
+        self.user = user
+
+    def __str__(self):
+        print_message = "[{}]: {} - {}".format(self.user.user_name,
+                                               self.message,
+                                               self.time)
+        return print_message
+
+
+DEV_SERVER_ADDR = "169.254.128.210"
+DEV_SERVER_PORT = 5000
